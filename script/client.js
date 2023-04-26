a3ae40570e4f85ea6c229f405e2ea082b5a896ee
Sergio Nicolas Pinilla <spinillac28@gmail.com>
2023-04-25 19:34:06 -0500

    Car terminado

--- script/client.js
+++ script/client.js
@@ -4,7 +4,10 @@ const inputName = document.getElementById("inputName")
 const inputEmail = document.getElementById("inputEmail")
 const inputPassword = document.getElementById("inputPassword")
 const inputAge = document.getElementById("inputAge")
+const contenedorId = document.getElementById("contenedorId")
 const btnModalFooter = document.getElementsByClassName("btn-modal-footer")
+
+
 
 function obtener(){
     var requestOptions = {
@@ -63,27 +66,77 @@ function eliminar(){
 }
 
 function actualizar(){
-    
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+    "idClient": inputId.value,
+    "name": inputName.value,
+    "email": inputEmail.value,
+    "password": inputPassword.value,
+    "age": inputAge.value
+    });
+
+    var requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("http://localhost/api/Client/update", requestOptions)
+    .then(response => {
+        window.location.reload()
+    })
+    .catch(error => console.log('error', error));
 }
 
 function crear(){
-    
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+    "name": inputName.value,
+    "email": inputEmail.value,
+    "password": inputPassword.value,
+    "age": inputAge.value
+    });
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("http://localhost/api/Client/save", requestOptions)
+    .then(response => {
+        window.location.reload()
+    })
+    .catch(error => console.log('error', error));
 }
 
 function limpiarInput(){
-
+    inputId.value = null
+    inputName.value = null
+    inputEmail.value = null
+    inputPassword.value = null
+    inputAge.value = null
 }
 
 function btnAdd(){
     btnModalFooter[0].style.display = "block"
     btnModalFooter[1].style.display = "none"
     btnModalFooter[2].style.display = "none"
+    contenedorId.style.display = "none"
+    limpiarInput()
 }
 
 function btnDetails(id){
     btnModalFooter[0].style.display = "none"
     btnModalFooter[1].style.display = "block"
     btnModalFooter[2].style.display = "block"
+    contenedorId.style.display = "block"
     obtenerPorId(id)
 }
 
